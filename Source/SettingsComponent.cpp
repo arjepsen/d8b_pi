e0d65f955a4ecff666d6aa59083a531316ea69ab
arjepsen <arjepsen@gmail.com>
2023-05-18 14:38:09 +0200

    fix alert window on wrong usb port choice.

--- Source/SettingsComponent.cpp
+++ Source/SettingsComponent.cpp
@@ -22,138 +22,120 @@
 
 #include "SettingsComponent.h"
 
-
 //[MiscUserDefs] You can add your own user definitions and misc code here...
 //[/MiscUserDefs]
 
 //==============================================================================
-SettingsComponent::SettingsComponent (MixerManager& mixerManagerInstance)
+SettingsComponent::SettingsComponent(MixerManager &mixerManagerInstance)
     : mixerManager(mixerManagerInstance)
 {
     //[Constructor_pre] You can add your own custom stuff here..
     //[/Constructor_pre]
 
-    brainPortComboBox.reset (new juce::ComboBox ("brainPortComboBox"));
-    addAndMakeVisible (brainPortComboBox.get());
-    brainPortComboBox->setEditableText (false);
-    brainPortComboBox->setJustificationType (juce::Justification::centredLeft);
-    brainPortComboBox->setTextWhenNothingSelected (juce::String());
-    brainPortComboBox->setTextWhenNoChoicesAvailable (TRANS("(no choices)"));
-    brainPortComboBox->addListener (this);
+    brainPortComboBox.reset(new juce::ComboBox("brainPortComboBox"));
+    addAndMakeVisible(brainPortComboBox.get());
+    brainPortComboBox->setEditableText(false);
+    brainPortComboBox->setJustificationType(juce::Justification::centredLeft);
+    brainPortComboBox->setTextWhenNothingSelected(juce::String());
+    brainPortComboBox->setTextWhenNoChoicesAvailable(TRANS("(no choices)"));
+    brainPortComboBox->addListener(this);
 
-    brainPortComboBox->setBounds (8, 32, 150, 24);
+    brainPortComboBox->setBounds(8, 32, 150, 24);
 
-    brainPortLabel.reset (new juce::Label ("brainPortLabel",
-                                           TRANS("Brain Port\n")));
-    addAndMakeVisible (brainPortLabel.get());
-    brainPortLabel->setFont (juce::Font (15.00f, juce::Font::plain).withTypefaceStyle ("Regular"));
-    brainPortLabel->setJustificationType (juce::Justification::centredLeft);
-    brainPortLabel->setEditable (false, false, false);
-    brainPortLabel->setColour (juce::TextEditor::textColourId, juce::Colours::black);
-    brainPortLabel->setColour (juce::TextEditor::backgroundColourId, juce::Colour (0x00000000));
+    brainPortLabel.reset(new juce::Label("brainPortLabel",
+                                         TRANS("Brain Port\n")));
+    addAndMakeVisible(brainPortLabel.get());
+    brainPortLabel->setFont(juce::Font(15.00f, juce::Font::plain).withTypefaceStyle("Regular"));
+    brainPortLabel->setJustificationType(juce::Justification::centredLeft);
+    brainPortLabel->setEditable(false, false, false);
+    brainPortLabel->setColour(juce::TextEditor::textColourId, juce::Colours::black);
+    brainPortLabel->setColour(juce::TextEditor::backgroundColourId, juce::Colour(0x00000000));
 
-    brainPortLabel->setBounds (8, 8, 79, 24);
+    brainPortLabel->setBounds(8, 8, 79, 24);
 
-    dspPortComboBox.reset (new juce::ComboBox ("dspPortComboBox"));
-    addAndMakeVisible (dspPortComboBox.get());
-    dspPortComboBox->setEditableText (false);
-    dspPortComboBox->setJustificationType (juce::Justification::centredLeft);
-    dspPortComboBox->setTextWhenNothingSelected (juce::String());
-    dspPortComboBox->setTextWhenNoChoicesAvailable (TRANS("(no choices)"));
-    dspPortComboBox->addListener (this);
+    dspPortComboBox.reset(new juce::ComboBox("dspPortComboBox"));
+    addAndMakeVisible(dspPortComboBox.get());
+    dspPortComboBox->setEditableText(false);
+    dspPortComboBox->setJustificationType(juce::Justification::centredLeft);
+    dspPortComboBox->setTextWhenNothingSelected(juce::String());
+    dspPortComboBox->setTextWhenNoChoicesAvailable(TRANS("(no choices)"));
+    dspPortComboBox->addListener(this);
 
-    dspPortComboBox->setBounds (176, 32, 150, 24);
+    dspPortComboBox->setBounds(176, 32, 150, 24);
 
-    dspPortLabel.reset (new juce::Label ("dspPortLabel",
-                                         TRANS("DSP Port\n")));
-    addAndMakeVisible (dspPortLabel.get());
-    dspPortLabel->setFont (juce::Font (15.00f, juce::Font::plain).withTypefaceStyle ("Regular"));
-    dspPortLabel->setJustificationType (juce::Justification::centredLeft);
-    dspPortLabel->setEditable (false, false, false);
-    dspPortLabel->setColour (juce::TextEditor::textColourId, juce::Colours::black);
-    dspPortLabel->setColour (juce::TextEditor::backgroundColourId, juce::Colour (0x00000000));
+    dspPortLabel.reset(new juce::Label("dspPortLabel",
+                                       TRANS("DSP Port\n")));
+    addAndMakeVisible(dspPortLabel.get());
+    dspPortLabel->setFont(juce::Font(15.00f, juce::Font::plain).withTypefaceStyle("Regular"));
+    dspPortLabel->setJustificationType(juce::Justification::centredLeft);
+    dspPortLabel->setEditable(false, false, false);
+    dspPortLabel->setColour(juce::TextEditor::textColourId, juce::Colours::black);
+    dspPortLabel->setColour(juce::TextEditor::backgroundColourId, juce::Colour(0x00000000));
 
-    dspPortLabel->setBounds (176, 8, 79, 24);
+    dspPortLabel->setBounds(176, 8, 79, 24);
 
-    deviceListLabel.reset (new juce::Label ("deviceListLabel",
-                                            juce::String()));
-    addAndMakeVisible (deviceListLabel.get());
-    deviceListLabel->setFont (juce::Font (15.00f, juce::Font::plain).withTypefaceStyle ("Regular"));
-    deviceListLabel->setJustificationType (juce::Justification::topLeft);
-    deviceListLabel->setEditable (false, false, false);
-    deviceListLabel->setColour (juce::Label::outlineColourId, juce::Colour (0x9ec4c4c4));
-    deviceListLabel->setColour (juce::TextEditor::textColourId, juce::Colours::black);
-    deviceListLabel->setColour (juce::TextEditor::backgroundColourId, juce::Colour (0x00000000));
+    deviceListLabel.reset(new juce::Label("deviceListLabel",
+                                          juce::String()));
+    addAndMakeVisible(deviceListLabel.get());
+    deviceListLabel->setFont(juce::Font(15.00f, juce::Font::plain).withTypefaceStyle("Regular"));
+    deviceListLabel->setJustificationType(juce::Justification::topLeft);
+    deviceListLabel->setEditable(false, false, false);
+    deviceListLabel->setColour(juce::Label::outlineColourId, juce::Colour(0x9ec4c4c4));
+    deviceListLabel->setColour(juce::TextEditor::textColourId, juce::Colours::black);
+    deviceListLabel->setColour(juce::TextEditor::backgroundColourId, juce::Colour(0x00000000));
 
-    deviceListLabel->setBounds (8, 96, 320, 104);
+    deviceListLabel->setBounds(8, 96, 320, 104);
 
-    comPortListLabel.reset (new juce::Label ("comPortListLabel",
-                                             TRANS("COM port device list\n")));
-    addAndMakeVisible (comPortListLabel.get());
-    comPortListLabel->setFont (juce::Font (15.00f, juce::Font::plain).withTypefaceStyle ("Regular"));
-    comPortListLabel->setJustificationType (juce::Justification::centredLeft);
-    comPortListLabel->setEditable (false, false, false);
-    comPortListLabel->setColour (juce::TextEditor::textColourId, juce::Colours::black);
-    comPortListLabel->setColour (juce::TextEditor::backgroundColourId, juce::Colour (0x00000000));
+    comPortListLabel.reset(new juce::Label("comPortListLabel",
+                                           TRANS("COM port device list\n")));
+    addAndMakeVisible(comPortListLabel.get());
+    comPortListLabel->setFont(juce::Font(15.00f, juce::Font::plain).withTypefaceStyle("Regular"));
+    comPortListLabel->setJustificationType(juce::Justification::centredLeft);
+    comPortListLabel->setEditable(false, false, false);
+    comPortListLabel->setColour(juce::TextEditor::textColourId, juce::Colours::black);
+    comPortListLabel->setColour(juce::TextEditor::backgroundColourId, juce::Colour(0x00000000));
 
-    comPortListLabel->setBounds (8, 72, 176, 24);
+    comPortListLabel->setBounds(8, 72, 176, 24);
 
-    brainBaudRateLabel.reset (new juce::Label ("brainBaudRateLabel",
-                                               TRANS("Brain Baudrate\n")));
-    addAndMakeVisible (brainBaudRateLabel.get());
-    brainBaudRateLabel->setFont (juce::Font (15.00f, juce::Font::plain).withTypefaceStyle ("Regular"));
-    brainBaudRateLabel->setJustificationType (juce::Justification::centredLeft);
-    brainBaudRateLabel->setEditable (false, false, false);
-    brainBaudRateLabel->setColour (juce::TextEditor::textColourId, juce::Colours::black);
-    brainBaudRateLabel->setColour (juce::TextEditor::backgroundColourId, juce::Colour (0x00000000));
+    brainBaudRateLabel.reset(new juce::Label("brainBaudRateLabel",
+                                             TRANS("Brain Baudrate\n")));
+    addAndMakeVisible(brainBaudRateLabel.get());
+    brainBaudRateLabel->setFont(juce::Font(15.00f, juce::Font::plain).withTypefaceStyle("Regular"));
+    brainBaudRateLabel->setJustificationType(juce::Justification::centredLeft);
+    brainBaudRateLabel->setEditable(false, false, false);
+    brainBaudRateLabel->setColour(juce::TextEditor::textColourId, juce::Colours::black);
+    brainBaudRateLabel->setColour(juce::TextEditor::backgroundColourId, juce::Colour(0x00000000));
 
-    brainBaudRateLabel->setBounds (8, 224, 176, 24);
+    brainBaudRateLabel->setBounds(8, 224, 176, 24);
 
-    brainBaudRateComboBox.reset (new juce::ComboBox ("brainBaudRateComboBox"));
-    addAndMakeVisible (brainBaudRateComboBox.get());
-    brainBaudRateComboBox->setEditableText (false);
-    brainBaudRateComboBox->setJustificationType (juce::Justification::centredLeft);
-    brainBaudRateComboBox->setTextWhenNothingSelected (juce::String());
-    brainBaudRateComboBox->setTextWhenNoChoicesAvailable (TRANS("(no choices)"));
-    brainBaudRateComboBox->addItem (TRANS("115200"), 1);
-    brainBaudRateComboBox->addItem (TRANS("230400"), 2);
-    brainBaudRateComboBox->addListener (this);
+    brainBaudRateComboBox.reset(new juce::ComboBox("brainBaudRateComboBox"));
+    addAndMakeVisible(brainBaudRateComboBox.get());
+    brainBaudRateComboBox->setEditableText(false);
+    brainBaudRateComboBox->setJustificationType(juce::Justification::centredLeft);
+    brainBaudRateComboBox->setTextWhenNothingSelected(juce::String());
+    brainBaudRateComboBox->setTextWhenNoChoicesAvailable(TRANS("(no choices)"));
+    brainBaudRateComboBox->addItem(TRANS("115200"), 1);
+    brainBaudRateComboBox->addItem(TRANS("230400"), 2);
+    brainBaudRateComboBox->addListener(this);
 
-    brainBaudRateComboBox->setBounds (8, 248, 150, 24);
+    brainBaudRateComboBox->setBounds(8, 248, 150, 24);
 
-    initMixerBtn.reset (new juce::TextButton ("initMixerBtn"));
-    addAndMakeVisible (initMixerBtn.get());
-    initMixerBtn->setButtonText (TRANS("Initialize Mixer"));
-    initMixerBtn->addListener (this);
+    initMixerBtn.reset(new juce::TextButton("initMixerBtn"));
+    addAndMakeVisible(initMixerBtn.get());
+    initMixerBtn->setButtonText(TRANS("Initialize Mixer"));
+    initMixerBtn->addListener(this);
 
-    initMixerBtn->setBounds (200, 248, 150, 24);
-
+    initMixerBtn->setBounds(200, 248, 150, 24);
 
     //[UserPreSize]
     //[/UserPreSize]
 
-    setSize (400, 300);
-
+    setSize(400, 300);
 
     //[Constructor] You can add your own custom stuff here..
 
     // Add the USB devices to the comboboxes, and populate the deviceListLabel with descriptions
     updatePortInfo();
-
-    // std::string deviceInfoText;
-    // for (const auto &device : mixerManager.getSettings().getUSBDevices())
-    // {
-    //     // brainPortComboBox->addItem(device.first + " (" + device.second + ")", brainPortComboBox->getNumItems() + 1);
-    //     // dspPortComboBox->addItem(device.first + " (" + device.second + ")", dspPortComboBox->getNumItems() + 1);
-    //     brainPortComboBox->addItem(device.first, brainPortComboBox->getNumItems() + 1);
-    //     dspPortComboBox->addItem(device.first, dspPortComboBox->getNumItems() + 1);
-
-    //     //kurt.append(device.first + ": " + device.second + "\n");
-
-    //     deviceInfoText.append(device.first + ":\n" + device.second + "\n");
-
-    // }
-    // deviceListLabel->setText(deviceInfoText, juce::dontSendNotification);
 
     //[/Constructor]
 }
@@ -173,18 +155,17 @@ SettingsComponent::~SettingsComponent()
     brainBaudRateComboBox = nullptr;
     initMixerBtn = nullptr;
 
-
     //[Destructor]. You can add your own custom destruction code here..
     //[/Destructor]
 }
 
 //==============================================================================
-void SettingsComponent::paint (juce::Graphics& g)
+void SettingsComponent::paint(juce::Graphics &g)
 {
     //[UserPrePaint] Add your own custom painting code here..
     //[/UserPrePaint]
 
-    g.fillAll (juce::Colour (0xff323e44));
+    g.fillAll(juce::Colour(0xff323e44));
 
     //[UserPaint] Add your own custom painting code here..
     //[/UserPaint]
@@ -199,7 +180,7 @@ void SettingsComponent::resized()
     //[/UserResized]
 }
 
-void SettingsComponent::comboBoxChanged (juce::ComboBox* comboBoxThatHasChanged)
+void SettingsComponent::comboBoxChanged(juce::ComboBox *comboBoxThatHasChanged)
 {
     //[UsercomboBoxChanged_Pre]
     //[/UsercomboBoxChanged_Pre]
@@ -248,7 +229,7 @@ void SettingsComponent::comboBoxChanged (juce::ComboBox* comboBoxThatHasChanged)
     //[/UsercomboBoxChanged_Post]
 }
 
-void SettingsComponent::buttonClicked (juce::Button* buttonThatWasClicked)
+void SettingsComponent::buttonClicked(juce::Button *buttonThatWasClicked)
 {
     //[UserbuttonClicked_Pre]
     //[/UserbuttonClicked_Pre]
@@ -257,23 +238,31 @@ void SettingsComponent::buttonClicked (juce::Button* buttonThatWasClicked)
     {
         //[UserButtonCode_initMixerBtn] -- add your button handler code here..
 
-        // TODO:
-        // FIRST CHECK IF WE HAVE SET THE USB PORTS!!!!!
-        // IF NOT, LET THE USER KNOW OF THEIR INCOMPETENCE.
-
-
-        initMixerBtn->setEnabled(false);    // Disable the button until init has finished.
+        // Check for incorrect port assignment.
+        std::string brainPort = mixerManager.getBrainPort();
+        std::string dspPort = mixerManager.getDspPort();
+        if (brainPort == "" || dspPort == "" || brainPort == dspPort)
+        {
+            juce::AlertWindow::showMessageBoxAsync(juce::AlertWindow::InfoIcon,
+                                                   "Wrong Selection",
+                                                   "Choose correct USB ports!",
+                                                   "OK");
+        }
+        else
+        {
+            // Disable the button, untill init has finished.
+            initMixerBtn->setEnabled(false); 
+            
+            // Call the init method in mixermanager - provide it with a pointer to the button.
+            mixerManager.initMixer(initMixerBtn.get());
+        }
         
-        // Call the init method in mixermanager - provide it with a pointer to the button.
-        mixerManager.initMixer(initMixerBtn.get());
         //[/UserButtonCode_initMixerBtn]
     }
 
     //[UserbuttonClicked_Post]
     //[/UserbuttonClicked_Post]
 }
-
-
 
 //[MiscUserCode] You can add your own definitions of your custom methods or any other code here...
 void SettingsComponent::updatePortInfo()
@@ -298,11 +287,8 @@ void SettingsComponent::updatePortInfo()
         deviceInfoText.append(device.first + ":\n" + device.second + "\n");
     }
     deviceListLabel->setText(deviceInfoText, juce::dontSendNotification);
-
-
 }
 //[/MiscUserCode]
-
 
 //==============================================================================
 #if 0
@@ -363,7 +349,5 @@ END_JUCER_METADATA
 */
 #endif
 
-
 //[EndFile] You can add extra defines here...
 //[/EndFile]
-
