72226b1e3a9abe9da55dc8834e24078af40b269d
arjepsen <arjepsen@gmail.com>
2025-05-17 18:50:27 +0200

    changing to "ControlInterface" instead of chstripinterface

--- Source/FatChannelClass.h
+++ Source/FatChannelClass.h
@@ -8,4 +8,24 @@
   ==============================================================================
 */
 
+#include "ChannelClass.h"
+#include "ChannelStripClass.h"
+
 #pragma once
+
+class FatChannel
+{
+  public:
+    // clang-format off
+    enum State { EQ, GATE, COMPRESSOR, PLUGIN, SETTINGS};
+    // clang-format on
+
+    FatChannel() : selectedChannelStrip(nullptr), selectedChannel(nullptr), state(EQ) {}
+
+    void setState(State newState) { state = newState; };
+
+
+
+  private:
+    State state = EQ;
+};
